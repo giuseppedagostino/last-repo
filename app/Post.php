eee9358b8a9db1fbad811c682e310c31ec898ab0
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-03-10 11:03:37 +0100

    Inclusione relazione nel Model Post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public $fillable = [
+        'title',
+        'slug',
+        'subtitle',
+        'image',
+        'author',
+        'content'
+    ];
+
+    // relazione db
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
 }
